157ec60f92488aa0fe536041f5874e1f758bb860
Erich Gubler <erichdongubler@gmail.com>
2024-08-07 16:36:03 -0400

    feat: add 10% margin around icon relative to nonant

--- src/main.rs
+++ src/main.rs
@@ -59,13 +59,34 @@ fn overlay_icon_over_pfp(profile_picture: &mut DynamicImage, icon: &DynamicImage
     use image::imageops::{overlay, overlay_bounds};
 
     // Divide the profile picture into an evenly spaced 3x3 grid. We want the icon to occupy the
-    // bottom-right nonant of the profile picture.
-    let x = 2 * profile_picture.width() / 3;
-    let y = 2 * profile_picture.height() / 3;
+    // bottom-right nonant of the profile picture. However, we also want a bit of padding, so take
+    // 10% of _that_ space on all sides and make it blank.
+    //
+    // Do all this by working in (1/3 nonant dimension divisions * 1/10 margin within a nonant) of
+    // the profile picture.
+    let icon_top_left_corner_coords = (
+        profile_picture.width() * 21 / 30,
+        profile_picture.height() * 21 / 30,
+    );
+    let icon_bottom_right_corner_coords = (
+        profile_picture.width() * 29 / 30,
+        profile_picture.height() * 29 / 30,
+    );
+
     let rescaled_icon = {
-        let (x, y) = overlay_bounds(profile_picture.dimensions(), icon.dimensions(), x, y);
+        let (x, y) = overlay_bounds(
+            icon_bottom_right_corner_coords,
+            icon.dimensions(),
+            icon_top_left_corner_coords.0,
+            icon_top_left_corner_coords.1,
+        );
         icon.resize(x, y, FilterType::Lanczos3)
     };
 
-    overlay(profile_picture, &rescaled_icon, x.into(), y.into())
+    overlay(
+        profile_picture,
+        &rescaled_icon,
+        icon_top_left_corner_coords.0.into(),
+        icon_top_left_corner_coords.1.into(),
+    )
 }
